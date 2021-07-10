ba96734ffbc973d3ed30fb1299b89ea2760fb7fe
krzpiszcz <kpiszcz@yahoo.com>
2021-07-10 17:31:57 +0200

    changed files

--- Python/Embed for your customers/AppOwnsData/config.py
+++ Python/Embed for your customers/AppOwnsData/config.py
@@ -7,25 +7,25 @@ class BaseConfig(object):
     AUTHENTICATION_MODE = 'ServicePrincipal'
 
     # Workspace Id in which the report is present
-    WORKSPACE_ID = ''
+    WORKSPACE_ID = '6a1a63ef-b919-4a74-b75e-9369e10ad67d'
     
     # Report Id for which Embed token needs to be generated
-    REPORT_ID = ''
+    REPORT_ID = '804134ae-f211-41ea-8faa-a12fa8ec4874'
     
     # Id of the Azure tenant in which AAD app and Power BI report is hosted. Required only for ServicePrincipal authentication mode.
-    TENANT_ID = ''
+    TENANT_ID = '7f565ca3-52a4-4afb-bd01-742df1734cf4'
     
     # Client Id (Application Id) of the AAD app
-    CLIENT_ID = ''
+    CLIENT_ID = 'bddcb30f-fb47-4f19-b2a4-14b2540cf535'
     
     # Client Secret (App Secret) of the AAD app. Required only for ServicePrincipal authentication mode.
-    CLIENT_SECRET = ''
+    CLIENT_SECRET = 'uX.R~oigrcMIJ2j1BMEsR6e4j3B-64D-Ti'
     
     # Scope of AAD app. Use the below configuration to use all the permissions provided in the AAD app through Azure portal.
     SCOPE = ['https://analysis.windows.net/powerbi/api/.default']
     
     # URL used for initiating authorization request
-    AUTHORITY = 'https://login.microsoftonline.com/organizations'
+    AUTHORITY = 'https://login.microsoftonline.com/organizations/'
     
     # Master user email address. Required only for MasterUser authentication mode.
     POWER_BI_USER = ''
